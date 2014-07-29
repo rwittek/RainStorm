019e64737f9a1ee2ce8c8fd616d6edb5987d76a7
rwittek <rwittekcodes@gmail.com>
2014-07-28 20:13:08 -0700

    mini fixes

--- src/cheats/bhop.rs
+++ src/cheats/bhop.rs
@@ -21,13 +21,12 @@ impl Cheat for Bunnyhop {
 		if !self.enabled {
 			return;
 		}
-		let localplayer_entidx = ptrs.ivengineclient.get_local_player();
-		let me = ptrs.icliententitylist.get_client_entity(localplayer_entidx).unwrap();
+		let me = sdk::utils::get_local_player_entity(ptrs);
+		
 		let flags: i32 = unsafe { *me.ptr_offset(0x0378) };
 		
 		if (flags & 1) == 0 && self.wasinair { // FL_ONGROUND
 			cmd.buttons &= !sdk::IN_JUMP;
-			cmd.upmove = 320.0;
 		}
 		self.wasinair = (flags & 1) == 0;
 		
